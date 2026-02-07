bac9d76df5e7a4b5004d7ff4ba1135f0f5d7b89a
xelphyr <xelphyrnull@gmail.com>
2026-02-07 12:03:33 +0530

    Added a bg gradient and started work on curved text

---
+++ src/lib/Hero.svelte
@@ -0,0 +1,11 @@
+<script>
+    import CurvedText from "$lib/text/curved_text.svelte";
+</script>
+
+<section class="panel">
+    <CurvedText 
+        displayText="partybox" 
+        font = "200px serif"
+        angle={Math.PI * 0.5} 
+        radius={360} />
+</section>
---
+++ src/lib/text/curved_text.svelte
@@ -0,0 +1,46 @@
+<script module>
+    let count = 0;
+    function getId() {
+        count++;
+        return `curved-text-${count}`;
+    }
+</script>
+
+<script lang="ts">
+	import { onMount } from "svelte";
+
+
+    let {displayText, font = "50px serif", angle=Math.PI*0.6, radius=200} = $props();
+
+    const id = getId();
+
+    let canvas: HTMLCanvasElement | null = null;
+    let context: CanvasRenderingContext2D | null = null;
+
+    onMount(() => {
+        if (!canvas) return;
+        context = canvas.getContext("2d");
+        if (!context) return;
+
+        context.font = font;
+        context.textAlign = "center";
+        context.fillStyle = "black";
+
+        context.translate(canvas.width / 2, canvas.height / 2 + radius/2);
+        context.rotate(-angle/2);
+
+        for (let i = 0; i < displayText.length; i++) {
+            const char = displayText[i];
+            context.rotate(angle / displayText.length);
+            context.save();
+            context.translate(0, -1 * radius);
+            context.fillText(char, 0, 0);
+            context.restore();
+        }
+    });
+</script>
+
+<canvas bind:this={canvas} id={id}
+        width = 1000 
+        height = 1000></canvas>
+
